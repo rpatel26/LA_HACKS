b0b4b5ebf177732da5ad706b960b33812909ef09
ababikir <ababikir@ucsd.edu>
2018-03-31 13:38:52 -0700

    Add files via upload
    This is Version_3 of our Python Code. 
    
    This version has all three effects: Pause/Resume, Volume Up/Down, Fade Out. 
    
    We also updated this version on the Google Drive as well.

--- Smart_Glove.py
+++ Smart_Glove.py
@@ -4,6 +4,8 @@ import sys
 #sys.exit()
 file = 'sample_ogg.ogg'
 command = 0
+volume_input = 0.5
+
 mixer.init()  # Initialize mixer
 mixer.music.load(file)  # Load the music file
 Playing = 1
@@ -11,6 +13,7 @@ mixer.music.play()      # Play the music file
 
 
 while mixer.music.get_busy():
+    # Pause and Play Effects
     glove_input = int(input("Pause or Play?"))
     command = glove_input
     if command:
@@ -20,3 +23,19 @@ while mixer.music.get_busy():
         else:
             mixer.music.unpause()
             Playing = 1
+
+    # Volume Up and Down Effects
+    volume_input = float(input("Input Volume Level"))
+    mixer.music.set_volume(volume_input)
+
+    # Fade Out Effect
+    fade_input = int(input("Fade out?"))  # Give 1 for fade out, 0 otherwise
+    if fade_input:
+        mixer.music.fadeout(5000)
+
+    #
+
+
+
+
+
